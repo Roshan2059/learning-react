c143fb88020f7140ed4277191fb4d204201fe21b
Roshan <roshanpanta2059@gmail.com>
2022-07-26 12:19:58 +0545

    Done upto the header of the expense tracker

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+function header() {
+    return (
+        <h2>Simple Expense Tracker</h2>
+    );
+}
+
+export default header;
---
+++ src/components/Intro.js
@@ -0,0 +1,6 @@
+function intro() {
+    return(
+        <h2>This is the first and very simple project I am going to do with react</h2>
+    );
+}
+export default intro;
