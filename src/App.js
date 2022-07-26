923b8b4a4232a790bec6d878e70229f59fae3e36
Roshan <roshanpanta2059@gmail.com>
2022-07-26 20:08:13 +0545

    Dont know how the file is modified

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import Intro from './components/Intro';
 import Header from './components/Header';
 import Mainbg from './components/mainbg';
 // import AnItem from './components/AnItem';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <h1>Hi!! 😃 I am Roshan</h1>
       <Intro />
-      <Header />
+      <Header /> I am a really kadaa person and I am a really kadaa person hahaha
       <Mainbg />
-
       <Footer />
     </div>
 
